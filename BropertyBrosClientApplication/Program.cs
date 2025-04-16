1d6f82472ace70c0c7dcecad113e4c7245e3fb48
Nayab Zafir <signorzafir@gmail.com>
2025-04-16 12:35:32 +0200

    Test Get RealtorFirm

--- BropertyBrosClientApplication/Program.cs
+++ BropertyBrosClientApplication/Program.cs
@@ -50,6 +50,7 @@ namespace BropertyBrosClientApplication
             builder.Services.AddScoped<RealtorService>();
             builder.Services.AddScoped<PropertyService>();
             builder.Services.AddScoped<CityService>();
+            builder.Services.AddScoped<RealtorFirmService>();
 
 
             var app = builder.Build();
