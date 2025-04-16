44d7e8cf1b9d5feb707c8b8636219fb564126037
twofourZ <uppenberg95@gmail.com>
2025-04-16 15:34:15 +0200

    fiasdf

---
+++ BropertyBrosClientApplication/DTO/PropertyDto/PropertySearchDto.cs
@@ -0,0 +1,24 @@
+﻿namespace BropertyBrosClientApplication.DTO.Properties
+{
+    public class PropertySearchDto
+    {
+        public int? MinPrice { get; set; } = null;
+        public int? MaxPrice { get; set; } = null;
+        public int? MinMonthlyFee { get; set; } = null;
+        public int? MaxMonthlyFee { get; set; } = null;
+        public int? MinYearlyFee { get; set; } = null;
+        public int? MaxYearlyFee { get; set; } = null;
+        public int? MinLivingAreaKvm { get; set; } = null;
+        public int? MaxLivingAreaKvm { get; set; } = null;
+        public int? MinAncillaryAreaKvm { get; set; } = null;
+        public int? MaxAncillaryAreaKvm { get; set; } = null;
+        public int? MinLandAreaKvm { get; set; } = null;
+        public int? MaxLandAreaKvm { get; set; } = null;
+        public int? MinNumberOfRooms { get; set; } = null;
+        public int? MaxNumberOfRooms { get; set; } = null;
+        public int? MinBuildYear { get; set; } = null;
+        public int? MaxBuildYear { get; set; } = null;
+        public int? CityId { get; set; } = null;
+        public int? CategoryId { get; set; } = null;
+    }
+}
