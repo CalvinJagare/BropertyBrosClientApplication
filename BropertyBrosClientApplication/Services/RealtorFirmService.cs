4f49a6c9cca06838baf0ceac61218c735b0da009
Nayab Zafir <signorzafir@gmail.com>
2025-04-16 15:57:50 +0200

    4

--- BropertyBrosClientApplication/Services/RealtorFirmService.cs
+++ BropertyBrosClientApplication/Services/RealtorFirmService.cs
@@ -9,7 +9,7 @@ namespace BropertyBrosClientApplication.Services
     {
         private readonly HttpClient _httpClient;
 
-        public RealtorFirmService(IHttpClientFactory clientFactory )
+        public RealtorFirmService(IHttpClientFactory clientFactory)
         {
             _httpClient = clientFactory.CreateClient("BropertyApi2.0");
         }
@@ -17,7 +17,15 @@ namespace BropertyBrosClientApplication.Services
         private async Task<T> SendRequestAsync<T>(Func<Task<HttpResponseMessage>> httpRequest)
         {
             var response = await httpRequest();
-            response.EnsureSuccessStatusCode();
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorContent = await response.Content.ReadAsStringAsync();
+                throw new ApplicationException($"API Error: {response.StatusCode}, Details: {errorContent}");
+            }
+            else
+            {
+                response.EnsureSuccessStatusCode();
+            }
             var content = await response.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<T>(content, new JsonSerializerOptions
             {
@@ -27,6 +35,10 @@ namespace BropertyBrosClientApplication.Services
         public async Task<List<RealtorFirmReadDto>> GetAllRealtorFirmsAsync()
         {
             return await SendRequestAsync<List<RealtorFirmReadDto>>(() => _httpClient.GetAsync("https://localhost:7151/api/RealtorFirm"));
+        }
+        public async Task<RealtorFirmReadDto> GetRealtorFirmByIdAsync(int id)
+        {
+            return await SendRequestAsync<RealtorFirmReadDto>(() => _httpClient.GetAsync($"https://localhost:7151/api/RealtorFirm/{id}"));
         }
 
     }
