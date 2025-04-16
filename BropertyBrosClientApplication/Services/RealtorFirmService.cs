c71a079278cbfe83264b199c5f5c024e3a61812e
Nayab Zafir <signorzafir@gmail.com>
2025-04-16 11:56:29 +0200

    1

--- BropertyBrosClientApplication/Services/RealtorFirmService.cs
+++ BropertyBrosClientApplication/Services/RealtorFirmService.cs
@@ -1,6 +1,33 @@
-﻿namespace BropertyBrosClientApplication.Services
+﻿using BropertyBrosClientApplication.DTO.RealtorFirm;
+using System.Text.Json;
+
+namespace BropertyBrosClientApplication.Services
 {
+    //Author: Nayab
+
     public class RealtorFirmService
     {
+        private readonly HttpClient _httpClient;
+
+        public RealtorFirmService(IHttpClientFactory clientFactory )
+        {
+            _httpClient = clientFactory.CreateClient("BropertyApi2.0");
+        }
+
+        private async Task<T> SendRequestAsync<T>(Func<Task<HttpResponseMessage>> httpRequest)
+        {
+            var response = await httpRequest();
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(content, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            })!;
+        }
+        public async Task<List<RealtorFirmReadDto>> GetAllRealtorFirmsAsync()
+        {
+            return await SendRequestAsync<List<RealtorFirmReadDto>>(() => _httpClient.GetAsync("https://localhost:7151/api/RealtorFirm"));
+        }
+
     }
 }
