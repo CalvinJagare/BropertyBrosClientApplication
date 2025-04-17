0bdd7ca9ef2ac8d2c386da390fe1346538196fbd
Cal <151396661+CalvinJagare@users.noreply.github.com>
2025-04-17 11:40:26 +0200

    Home Page Layout

---
+++ BropertyBrosClientApplication/wwwroot/app.js
@@ -0,0 +1,10 @@
+﻿function updateCarousel(activeIndex) {
+    const slides = document.querySelectorAll(".carousel-slide");
+    slides.forEach((slide, index) => {
+        if (index === activeIndex) {
+            slide.classList.add("active");
+        } else {
+            slide.classList.remove("active");
+        }
+    });
+}
