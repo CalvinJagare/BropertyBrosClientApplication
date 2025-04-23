2e2d72a40f501e04e29709bbd47ed7ad82e2e698
Cal <151396661+CalvinJagare@users.noreply.github.com>
2025-04-24 00:19:05 +0200

    test

---
+++ BropertyBrosClientApplication/DTO/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using BropertyBrosApi2._0.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace BropertyBrosClientApplication.DTO
+{
+    public class UserDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
