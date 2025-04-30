9c7b4ed70edcbfe6e25abbd9fa0e727adfb1ab8f
twofourZ <uppenberg95@gmail.com>
2025-04-30 14:58:33 +0200

    Funkish

--- BropertyBrosClientApplication/Services/Response.cs
+++ BropertyBrosClientApplication/Services/Response.cs
@@ -1,10 +1,14 @@
 ﻿namespace BropertyBrosClientApplication.Services
 {
-    public class ApiResponse<T>
+    public class ApiResponse
     {
         public string Message { get; set; } = string.Empty;
         public bool Success { get; set; } = false;
-        public T Data { get; set; } = default!;
         public string ValidationErrors { get; set; } = string.Empty;
+    }
+
+    public class ApiResponse<T> : ApiResponse
+    {
+        public T Data { get; set; } = default!;
     }
 }
